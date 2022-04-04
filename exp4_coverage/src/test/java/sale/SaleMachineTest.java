721a56d7ff992f96dbc579a54e02a22ec39d166f
chrisliu0810 <Legend1986985788@163.com>
2022-04-04 21:10:45 +0800

    update SaleMachine.sale() & add 路径覆盖测试

--- exp4_coverage/src/test/java/sale/SaleMachineTest.java
+++ exp4_coverage/src/test/java/sale/SaleMachineTest.java
@@ -7,17 +7,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class SaleMachineTest {
     SaleMachine saleMachine = new SaleMachine();
 
+    /**
+     * 路径覆盖测试用例
+     */
     @org.junit.jupiter.api.Test
     void sale() {
-//        assertEquals("请取走beer", saleMachine.sale("beer", 5));
-        assertEquals("很抱歉，没有beer", saleMachine.sale("beer", 5));
-        assertEquals("请取走beer", saleMachine.sale("beer", 5));
-        assertEquals("请取走orange", saleMachine.sale("orange", 5));
-        assertEquals("错误指令", saleMachine.sale("coke", 5));
-        assertEquals("没有零钱，退您十元", saleMachine.sale("beer", 10));
-        assertEquals("请取走beer", saleMachine.sale("beer", 10));
-        assertEquals("请取走orange", saleMachine.sale("orange", 10));
-        assertEquals("错误指令", saleMachine.sale("beer", 10));
-        assertEquals("错误指令", saleMachine.sale("coke", 11));
+        assertEquals("很抱歉，没有beer", saleMachine.sale("beer", 5, 5, 0, 0));
+        assertEquals("请取走beer", saleMachine.sale("beer", 5, 5, 1, 1));
+        assertEquals("请取走orange", saleMachine.sale("orange", 5, 5, 1, 1));
+        assertEquals("错误指令", saleMachine.sale("coke", 5, 5, 1, 1));
+        assertEquals("没有零钱，退您十元", saleMachine.sale("beer", 10, 0, 1, 1));
+        assertEquals("请取走beer", saleMachine.sale("beer", 10, 1, 1, 1));
+        assertEquals("请取走orange", saleMachine.sale("orange", 10, 1, 1, 1));
+        assertEquals("错误指令", saleMachine.sale("coke", 10, 1, 1, 1));
+        assertEquals("错误指令", saleMachine.sale("coke", 11, 1, 1, 1));
     }
 }
